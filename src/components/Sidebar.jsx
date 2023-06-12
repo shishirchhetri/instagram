8240b3f61bc28b6acceaccef39e8b7e6868dad98
spark <shishirchhetri45@gmail.com>
2023-06-12 20:09:38 +0545

    added icon active

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,67 +1,89 @@
-import React, { Children, useState } from "react";
+import React, { useState } from "react";
 import { BsInstagram } from "react-icons/bs";
-import { AiOutlineMenu, AiOutlineHeart } from "react-icons/ai";
-import { MdOutlineExplore, MdOutlineAddBox } from "react-icons/md";
-import { RiMessengerLine } from "react-icons/ri";
-import { GoSearch } from "react-icons/go";
+import { AiOutlineMenu, AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { MdOutlineExplore, MdOutlineAddBox,MdAddBox, MdExplore } from "react-icons/md";
+import { RiMessengerLine, RiMessengerFill } from "react-icons/ri";
+import {IoSearch, IoSearchSharp} from 'react-icons/io'
 import logo from "../assets/logo.png";
 import profile from "../assets/img2.jpg";
 import Topbar from "./Topbar";
 import Bottombar from "./Bottombar";
 import reels from "../assets/video.png";
-import home from "../assets/icons/home.png";
+import reelsActive from "../assets/videoFill.png";
+import home from "../assets/home.png";
+import homeFill from '../assets/homeActive.png'
+
 
 const Sidebar = () => {
+const [activenav, setActivenav] = useState('home');
+
+const handleActiveNav = (navItem)=>{
+  setActivenav(navItem);
+}
+
   return (
     <div>
       <div className="hidden md:flex flex-col justify-between h-screen wfull border-r max-w-[73px]  xl:max-w-[245px] ">
-        <ul className="flex flex-col capitalize  gap-2 p-[11px] px-[12px] rounded-md w-[245px] mt-1 ">
+        <ul className="flex flex-col capitalize  gap-2 p-[11px] px-[12px] rounded-md w-full max-w-[73px]  xl:max-w-[245px] mt-1 ">
           <div className="p-3 py-[26px]  w-full">
             <div className="hidden xl:inline">
               <img src={logo} alt="logo" />
             </div>
             <BsInstagram className="h-6 w-6 inline xl:hidden" />
           </div>
-          <li className="li-lg">
-            <img src={home} className="icon-comp" />
-            <span className="hidden xl:inline">Home</span>
+          <li className={`li-lg ${activenav ==='home' ? 'active' : ''}`} onClick={()=> handleActiveNav('home')}>
+            {
+              activenav === 'home' ? <img src={homeFill} className="icon-comp" /> : <img src={home} className="icon-comp" />
+            }
+            <span className="hidden xl:inline" >Home</span>
           </li>
 
-          <li className="li-lg">
-            <GoSearch className="icon-comp" title="Search" />
+          <li className={`li-lg ${activenav ==='search' ? 'active' : ''}`} onClick={()=> handleActiveNav('search')}>
+            { 
+              activenav === 'search' ? <IoSearchSharp className="icon-comp" title="Search" /> : <IoSearch className="icon-comp" title="Search" />
+            }
             <span className="hidden xl:inline">Search</span>
           </li>
-          <li className="li-lg">
-            <MdOutlineExplore className="icon-comp" title="Explore" />
+          <li className={`li-lg ${activenav ==='explore' ? 'active' : ''}`} onClick={()=> handleActiveNav('explore')}>
+            {activenav==='explore' ?<MdExplore className="icon-comp" title="Explore" />: <MdOutlineExplore className="icon-comp" title="Explore" />}
             <span className="hidden xl:inline">Explore</span>
           </li>
-          <li className="li-lg">
-            <img src={reels} alt="" className="icon-comp" />
+          <li className={`li-lg flex justify-center ${activenav ==='reels' ? 'active' : ''}`} onClick={()=> handleActiveNav('reels')}>
+            {
+              activenav==='reels' ? <img src={reelsActive} alt="" className="icon-comp" /> : <img src={reels} alt="" className="icon-comp" />
+
+            }
             <span className="hidden xl:inline">Reels</span>
           </li>
-          <li className="li-lg">
+          <li className={`li-lg ${activenav ==='messages' ? 'active' : ''}`} onClick={()=> handleActiveNav('messages')}>
             <div className="relative">
-              <RiMessengerLine className="icon-comp" title="Messages" />
+              {
+                activenav === 'messages' ? <RiMessengerFill className="icon-comp" title="Messages" />:<RiMessengerLine className="icon-comp" title="Messages" />
+              }
               <span className=" flex absolute bg-red-500 p-1 rounded-full h-[18px] w-[18px] items-center justify-center -top-1 left-4 text-white text-[10px]">
                 1
               </span>
             </div>
             <span className="hidden xl:inline">Messages</span>
           </li>
-          <li className="li-lg">
+          <li className={`li-lg ${activenav ==='notifications' ? 'active' : ''}`} onClick={()=> handleActiveNav('notifications')}>
             <div className="relative">
-              <AiOutlineHeart className="icon-comp" title="Notifications" />
+              {
+                activenav === 'notifications' ? <AiFillHeart className="icon-comp" title="Notifications" /> : <AiOutlineHeart className="icon-comp" title="Notifications" />
+              }
               <span className=" flex absolute bg-red-500 p-1 rounded-full h-[18px] w-[18px] items-center justify-center -top-1 left-4 text-white text-[10px]">
                 1
               </span>
             </div>
             <span className="hidden xl:inline">Notifications</span>
           </li>
-          <li className="li-lg">
-            <MdOutlineAddBox className="icon-comp" title="Create" />
+          <li className={`li-lg ${activenav ==='create' ? 'active' : ''}`} onClick={()=> handleActiveNav('create')}>
+            {
+              activenav === 'create' ? <MdAddBox className="icon-comp" title="Create" /> : <MdOutlineAddBox className="icon-comp" title="Create" />
+            }
             <span className="hidden xl:inline">Create</span>
           </li>
-          <li className="li-lg">
+          <li className={`li-lg ${activenav ==='profile' ? 'active' : ''}`} onClick={()=> handleActiveNav('profile')}>
             <img
               src={profile}
               className="icon-comp rounded-full"
